c4cb22cdd7801f517a0d2d34569ad762cee3be4e
davidmikulic03 <d.mikulic03@gmail.com>
2023-10-20 18:03:53 +0200

    David
    Creature AI stuff. Not pushing to main just yet because it doesn't quite work yet.

--- Assets/Scripts/Entities/Creature.cs
+++ Assets/Scripts/Entities/Creature.cs
@@ -1,5 +1,7 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem.HID;
+using static UnityEngine.UI.Image;
 
 [Serializable]
 public struct CreatureStats
@@ -24,7 +26,8 @@ public struct CreatureStats
 
 public enum CreatureState
 {
-    GROUNDED,
+    RUNNING,
+    WAITING,
     HELD,
     INACTIVE
 }
@@ -41,11 +44,14 @@ public class Creature : MonoBehaviour
     private float accelerationIncrement;
     private float decelerationIncrement;
 
+    private Vector3 targetPosition;
     private Vector3 direction = Vector3.forward;
     private Vector3 targetDirection = Vector3.forward;
 
     private float speed;
     private Vector3 velocity = Vector3.zero;
+
+    [SerializeField] LayerMask ignoreLayer;
 
 
     void Start()
@@ -74,8 +80,9 @@ public class Creature : MonoBehaviour
 
     public void FixedTick()
     {
-        Accelerate();
+        FindValidTarget();
         UpdateDirection();
+        ChooseInput();
         UpdateRotation();
         UpdateMovement();
     }
@@ -84,6 +91,52 @@ public class Creature : MonoBehaviour
     {
         Vector2 random2D = (UnityEngine.Random.insideUnitCircle).normalized;
         return new Vector3(random2D.x, 0, random2D.y);
+    }
+
+    void FindValidTarget()
+    {
+        bool proximityCondition = (targetPosition - transform.position).sqrMagnitude > 1;
+        bool clearPathCondition = !CastToTarget(ref targetPosition);
+
+        if (proximityCondition || (proximityCondition && clearPathCondition))
+            return;
+
+        int retryLimit = 50;
+        int retryIndex = 0;
+
+        while (retryIndex < retryLimit)
+        {
+            retryIndex++;
+
+            if (!CastToTarget(ref targetPosition))
+                break;
+        }
+        Debug.Log(retryIndex);
+    }
+
+    bool CastToTarget(ref Vector3 target)
+    {
+        Vector3 candidateTarget = CandidateTarget();
+        Vector3 origin = transform.position;
+        Vector3 direction = candidateTarget - transform.position;
+        bool output = Physics.Raycast(origin, direction, stats.viewRange, ~ignoreLayer);
+        if (output) target = candidateTarget;
+        return output;
+    }
+
+    Vector3 CandidateTarget()
+    {
+        Vector3 candidate = stats.viewRange * UnityEngine.Random.insideUnitCircle;
+        candidate = new Vector3(candidate.x, 0, candidate.y);
+        return candidate + transform.position.y * Vector3.up;
+    }
+
+    private void ChooseInput()
+    {
+        float dirDot = Vector3.Dot(direction, targetDirection);
+        if(dirDot > 0.5f)
+            Accelerate();
+        else Decelerate();
     }
 
     private void Accelerate()
@@ -108,7 +161,10 @@ public class Creature : MonoBehaviour
 
     private void UpdateDirection()
     {
+        targetDirection = (targetPosition - transform.position).normalized;
         direction = Vector3.RotateTowards(transform.forward, targetDirection, stats.turnRate * Time.fixedDeltaTime * Mathf.Deg2Rad, 0.0f);
+        direction.y = 0;
+        Vector3.Normalize(direction);
     }
 
     private void UpdateMovement()
@@ -120,5 +176,18 @@ public class Creature : MonoBehaviour
     private void UpdateRotation()
     {
         transform.forward = direction;
+    }
+
+    public void ChangeState(CreatureState state)
+    {
+        this.state = state;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, stats.viewRange);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(targetPosition, 0.5f);
     }
 }

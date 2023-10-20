2a72f7f986f6b420655dbc972e7ad0160e9e0df1
TheNiceOne346 <145574490+TheNiceOne346@users.noreply.github.com>
2023-10-20 15:04:43 +0200

    Test

--- Assets/Scripts/ScriptableObjects/PlayerStats.cs
+++ Assets/Scripts/ScriptableObjects/PlayerStats.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 
 [CreateAssetMenu(fileName = "PlayerStats", menuName = "Data/PlayerStats", order = 1)]
-public class PlayerStats : ScriptableObject {
 
-    [Range(1.0f, 10.0f)] public float turnRate = 120.0f;
-    [Range(1.0f, 999.0f)] public float maxSpeed = 260.0f;
-    [Range(1.0f, 999.0f)] public float accelerationRate = 20.0f;
-    [Range(1.0f, 999.0f)] public float decelerationRate = 10.0f;
+public class PlayerStats : ScriptableObject
+{
+    [Header("Movement")]
+    [Range(1.0f, 20.0f)] public float maxSpeed = 1.0f;
+    [Range(1.0f, 8.0f)] public float turnRate = 1.0f;
+    
+    [Header("Acceleration")]
+    [Range(1.0f, 200.0f)] public float accelerationspeed = 2.0f;
+    [Range(1.0f, 200f)] public float decelerationspeed = 1.0f;
+   
 
-}
+}    

880021632adf1cd8f1f61a67c9f3f4b6b244041a
LightPinkCheese <pinkcheese.ester@gmail.com>
2023-10-23 12:05:03 +0200

    Next to nothing
    Some things

--- Assets/Scripts/Menus/UISettingsMenu.cs
+++ Assets/Scripts/Menus/UISettingsMenu.cs
@@ -1,22 +1,40 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UISettingsMenu : MonoBehaviour
 {
     private bool initialize = false;
     private GameInstance gameInstance = null;
+    private SoundManager soundManager = null;
+    public Slider VolumeSlider;
 
 
-    public void Initialize(GameInstance reference)
+    public void Initialize(GameInstance gameInstance, SoundManager soundManager)
     {
         if (initialize)
             return;
 
-        gameInstance = reference;
+        this.gameInstance = gameInstance;
+        this.soundManager = soundManager;
         initialize = true;
     }
 
     public void ReturnFromSettings()
-    {
+    {   
+        if (gameInstance.IsGameStarted())
+        {
+            gameInstance.SetGameState(GameInstance.GameState.PLAYING);
+        }
+        else
+        {
+            gameInstance.SetGameState(GameInstance.GameState.MAIN_MENU);
+        }
+    }
 
+    public void SetVolumeSlider(float value)
+    {
+        //soundManager.setsfxvolume = value;
+        //soundManager.settrackvolume = value;
+        //soundManager.setmastervolume = value;
     }
 }
